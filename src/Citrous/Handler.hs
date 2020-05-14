9f28d5703ef04d4e055959c51e2879a8420dc77c
shintaro.sakata <shintaro.sakata.tokyo@gmail.com>
2020-05-15 01:34:12 +0900

    add minimal Handler class

---
+++ src/Citrous/Handler.hs
@@ -0,0 +1,9 @@
+module Citrous.Handler where
+
+import           Data.Functor.Identity (Identity, runIdentity)
+
+class Container c where
+  runHandler :: c a -> IO a
+
+instance Container Identity where
+  runHandler = return . runIdentity
