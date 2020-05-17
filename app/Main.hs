8c867eccdc0e5b999ed325f6805e79a9705b31c4
shintaro.sakata <shintaro.sakata.tokyo@gmail.com>
2020-05-17 23:37:29 +0900

    add example

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,31 @@
 {-# LANGUAGE DataKinds         #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 {-# LANGUAGE TypeApplications  #-}
 
 module Main where
 
-import           Citrous.Integration.Handler
-import           Citrous.Integration.HasHandler
-import           Citrous.Integration.Routes     (Routes, runRoutes)
-import           Citrous.Unit.Impl
-import           Citrous.Unit.MediaTypes
+import           Citrous.API
+import           Data.Aeson.TH                  (defaultOptions, deriveJSON)
 import           Data.Convertible.Utf8.Internal
-import           Data.Functor.Identity          (Identity)
 import           Network.Wai.Handler.Warp       (run)
+
+main :: IO ()
+main = run 8080 $ runRoutes routes
 
 routes :: Routes
 routes = do
   route @(Get '[TextPlain] Text) rootHandler
-  route @(Post '[TextPlain] String) htmlHandler
+  route @(Post '[JSON] User) userHandler
 
 rootHandler :: Handler Text
 rootHandler = return "Hello Citrous!"
 
-htmlHandler :: Handler String
-htmlHandler = return "<h1>String</h1>"
+userHandler :: Handler User
+userHandler = return $ User 24 "入田 関太郎"
 
-main :: IO ()
-main = run 8080 $ runRoutes routes
+data User = User
+    { age  :: Int
+    , name :: Text
+    }
+$(deriveJSON defaultOptions 'User)
---
+++ src/Citrous/API.hs
@@ -0,0 +1,18 @@
+module Citrous.API
+  ( module Citrous.Integration.HasHandler,
+    module Citrous.Integration.Handler,
+    module Citrous.Integration.Routes,
+    module Citrous.Unit.Capture,
+    module Citrous.Unit.Impl,
+    module Citrous.Unit.MediaTypes,
+    module Citrous.Unit.ServerErr,
+  )
+where
+
+import Citrous.Integration.Handler
+import Citrous.Integration.HasHandler
+import Citrous.Integration.Routes
+import Citrous.Unit.Capture
+import Citrous.Unit.Impl
+import Citrous.Unit.MediaTypes
+import Citrous.Unit.ServerErr
